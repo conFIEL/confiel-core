cbfbbdd2e232a98a7595972aadb53dbfc7abbb45
jjperezaguinaga <me@jjperezaguinaga.com>
2023-10-18 00:53:10 +0000

    feature: Breakdown documentation from original service page

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>conFIEL Core</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/conFIEL/confiel-core',
   },
   chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/conFIEL/confiel-core',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'conFIEL Core',
   },
 }
 
